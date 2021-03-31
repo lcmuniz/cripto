6f66e90792c49fb57c9cb5b6d6e128c967fc6f33
Luiz Carlos Muniz <lcmuniz@gmail.com>
2021-03-31 00:51:08 -0300

    Ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import { useRef, useState } from 'react';
+
+let number = Math.trunc(Math.random() * 100)
 
 function App() {
+
+  const [message, setMessage] = useState('')
+  const [atempts, setAtempts] = useState(0)
+
+  const guessInput = useRef()
+  const guessButton = useRef()
+  const restartLink = useRef()
+
+  const testGuess = () => {
+    const guess = Number(guessInput.current.value) 
+    if (guess < number) {
+      setMessage('Seu palpite é muito pequeno!')
+    }
+    else if (guess > number) {
+      setMessage('Seu palpite é muito grande!')
+    }
+    else {
+      setMessage('Você acertou!')
+      restartLink.current.className = "restart visible"
+      guessButton.current.disabled = 'true'
+      guessInput.current.disabled = true
+    }
+    setAtempts(atempts + 1)
+  }
+
+  const restart = () => {
+    setAtempts(0)
+    number = Math.trunc(Math.random() * 100)
+    guessInput.current.value = ''
+    restartLink.current.className = "restart invisible"
+    guessButton.current.disabled = false
+    guessInput.current.disabled = false
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <p class="prompt">Adivinhe o número que eu estou pensando?</p>
+      <div className="form">
+        <input type="text" ref={guessInput} />
+        <button ref={guessButton} onClick={testGuess}>Enviar</button>
+      </div>
+      <span>{message}</span>
+      <a ref={restartLink} className="restart invisible" href="#" onClick={restart}>Clique aqui para reiniciar.</a>
+      <p>Número de tentativas: {atempts}</p>
     </div>
   );
 }
